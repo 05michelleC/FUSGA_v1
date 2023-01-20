f667f303ace5f56fd938eec0bdfec2a461021a0a
miche <michellecamelo50@gmail.com>
2023-01-20 16:48:24 -0500

    Prueba version 1 de FUSGA.

---
+++ app/src/main/java/com/example/fusga_v1/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.fusga_v1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView iv_logo1;
+    private TextView tv_version;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        iv_logo1 = (ImageView)findViewById(R.id.iv_logo1);
+        tv_version = (TextView) findViewById(R.id.tv_version);
+        /*
+           Parámetro (fullscreen)
+         */
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        /*
+           Crear una instancia(de objeto) de animaciones
+         */
+        Animation animation1 = AnimationUtils.loadAnimation(this,R.anim.iv_logo1);
+        ImageView logo = findViewById(R.id.iv_logo1);
+        logo.setAnimation(animation1);
+
+        Animation animation2 = AnimationUtils.loadAnimation(this,R.anim.version_anim);
+        TextView version = findViewById(R.id.tv_version);
+        version.setAnimation(animation2);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+    }
+}
---
+++ app/src/main/java/com/example/fusga_v1/MainActivity2.java
@@ -0,0 +1,18 @@
+package com.example.fusga_v1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        //Icono en el Action Bar
+        /*getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher);*/
+    }
+}
